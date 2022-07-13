ddb408a1387ff5c87966ceea59e6dc48fee53f53
Evgenii Kolesnikov <jekow@yandex.ru>
2022-07-13 14:47:47 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+module.exports = function towelSort(matrix) {
+    if (typeof matrix !== "object") return [];
+    if (matrix.length <= 1) return [];
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    return matrix
+        .map((el, i) => (i % 2 === 0 ? el.join(", ") : el.reverse().join(", ")))
+        .join(", ")
+        .split(", ")
+        .map((el) => Number(el));
+};
